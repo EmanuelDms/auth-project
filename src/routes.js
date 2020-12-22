ef25632784ff7cfd382c8fd20fca0831a00dc894
EmanuelDms <emanuel.duarte.maia@gmail.com>
2020-12-22 19:32:58 -0300

    feat: login jwt route post

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,10 @@
+import UserController from "./controllers/UserController";
+import AuthController from "./controllers/AuthController";
 import { Router } from "express";
 
 const routes = new Router();
+
+routes.post('/auth', AuthController.store);
+routes.post('/users', UserController.store);
 
 export default routes;
