c7759139f4736c793703468ed5e9136d174a1378
EmanuelDms <emanuel.duarte.maia@gmail.com>
2020-12-27 20:13:52 -0300

    docs: add comments

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -5,31 +5,43 @@ import jwtConfig from "../config/jwt";
 
 class AuthController{
   async store(req, res){
+    // a output text
+    let txt = 'Credentials does not match.';
+
+    // get data of request body
     const { email, password } = req.body;
 
+    // Search information about a user, using req.body email
     const user = await User.findOne({email});
 
+    // if user doesn't exist, returns a message
     if (!user) {
       return res
         .status(400)
-        .json({error: 'Credentials does not match.'})
+        .json({error: txt})
     }
 
+    // Compares req.body password matches with database user password
     const checkPassword = await bcryptjs.compare(password, user.password);
 
+    // if password doesn't match, returns a message
     if (!checkPassword) {
       return res
         .status(400)
-        .json({error: 'Credentials does not match.'});
+        .json({error: txt});
     }
 
+    // setting jsonwebtoken
     const { secret, expiresIn } = jwtConfig;
 
+    // Set payload attribute (jsonwebtoken)
     const token = jwt.sign({}, secret, {
+      // user id (string)
       subject:String(user._id),
       expiresIn
     });
 
+    // return user and its token
     return res.json({user:user.show(), token})
   }
 }
